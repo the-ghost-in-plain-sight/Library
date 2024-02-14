9f7ad080b12dd2146f245c0a8cc9dddf2b3719d2
the-ghost-in-plain-sight <84908458+the-ghost-in-plain-sight@users.noreply.github.com>
2024-02-14 15:22:00 +0100

    Refractored Js to use class instead of plain constructor

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 let myLibrary = [];
 
 /* Book constructor */
-function Book (title, author, pages, read) {
+/* function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+} */
+
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 /* function that pushes book to myLibrary */
